6e357b9b9b88467d7439917e5562c86904c6e94d
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-06-26 14:56:04 +0000

    create listing factory method

---
+++ AppwriteConsole.App/Helpers/FakerFactory.cs
@@ -0,0 +1,24 @@
+using Faker;
+using FakerBoolean = Faker.Boolean;
+using Models;
+
+public static class FakerFactory
+{
+  public static Listing GenerateListing()
+  {
+    var listing = new Listing
+    {
+      Title = Lorem.Sentence(3),
+      Description = Lorem.Paragraph(2),
+      Location = Address.City(),
+      Price = RandomNumber.Next(0, 10001), // Assuming Price is an integer
+      Bedrooms = RandomNumber.Next(1, 11), // Assuming Bedrooms is an integer
+      PetsAllowed = FakerBoolean.Random(),
+      ParkingAvailable = FakerBoolean.Random(),
+      AvailableDate = DateTime.Now.AddDays(RandomNumber.Next(0, 365)).ToString("yyyy-MM-dd"),
+      HousingType = new[] { "Apt", "House" }[RandomNumber.Next(0, 2)] // Randomly selects between "Apt" and "House"
+    };
+
+    return listing;
+  }
+}
