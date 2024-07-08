701a11e3687ca7e8bd542978845ef1cb0d52c3d4
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-07-08 22:55:24 +0000

    refactor: create temp abstractions for main logic

--- AppwriteConsole.App/Program.cs
+++ AppwriteConsole.App/Program.cs
@@ -8,91 +8,89 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        string environmentName = CommandLineArgumentParser.GetArgumentValue(args, "--environment");
-        ConfigurationHelper configHelper = new ConfigurationHelper(environmentName);
-        var databaseId = configHelper.GetSetting("DATABASE_ID");
-
-        if (string.IsNullOrEmpty(databaseId))
-        {
-            Console.WriteLine("Database ID is not set in the app settings.");
-            Environment.Exit(1);
-        }
-
-        var shouldConfirmSettings = UserSelection.GetBooleanAnswer("Would you like to confirm application settings?");
-
-        if (shouldConfirmSettings) configHelper.PrintSettings();
-
+        ConfigurationHelper configHelper = new ConfigurationHelper();
         AppwriteService appwriteService = new AppwriteService(configHelper.GetSettings());
 
+        var databaseId = GetDatabaseIdOrExit(configHelper);
         var databaseResponse = await appwriteService.GetDatabase(databaseId);
         bool databaseExists = databaseResponse.Result is not null;
 
         Console.WriteLine($"Database '{databaseId}' {(databaseExists ? "does" : "does not ")} exist");
 
-        // string databasePrompt = $"Would you like to {(databaseExists ? "reset" : "create")} the database from the local schema definition?";
-        // var shouldResetDatabase = UserSelection.GetBooleanAnswer(databasePrompt);
+        await PromptForDatabaseReset(databaseExists, databaseId, appwriteService, databaseResponse);
 
-        // if (shouldResetDatabase)
-        // {
-        //     if (databaseExists)
-        //     {
-        //         Console.WriteLine("Deleting existing database...");
-        //         await appwriteService.DeleteDatabase(databaseId);
-        //     }
+        await PromptForCollectionOperations(appwriteService, databaseId, databaseResponse);
+    }
 
-        //     try
-        //     {
-        //         string json = File.ReadAllText(Path.Combine("DDL", "housingsearch.json"));
-        //         SeedDatabaseDTO? seedDatabaseDTO = JsonSerializer.Deserialize<SeedDatabaseDTO>(json);
+    private static string GetDatabaseIdOrExit(ConfigurationHelper configHelper)
+    {
+        var databaseId = configHelper.GetSetting("DATABASE_ID");
+        if (string.IsNullOrEmpty(databaseId))
+        {
+            Console.WriteLine("Database ID is not set in the app settings.");
+            Environment.Exit(1);
+        }
+        return databaseId;
+    }
 
-        //         if (seedDatabaseDTO is null)
-        //         {
-        //             throw new JsonException();
-        //         }
+    private static async Task PromptForDatabaseReset(bool databaseExists, string databaseId, AppwriteService appwriteService, DatabaseResponse<Appwrite.Models.Database> databaseResponse)
+    {
+        string databasePrompt = $"Would you like to {(databaseExists ? "reset" : "create")} the database from the local schema definition?";
+        var shouldResetDatabase = UserSelection.GetBooleanAnswer(databasePrompt);
 
-        //         Console.WriteLine("Creating database...");
-        //         await appwriteService.CreateDatabase(new DatabaseDTO { DatabaseId = databaseId, Name = seedDatabaseDTO.DatabaseName });
+        if (!shouldResetDatabase) return;
 
-        //         Console.WriteLine("Creating collections...");
-        //         await appwriteService.CreateCollections(databaseId, seedDatabaseDTO.Collections);
+        if (databaseExists)
+        {
+            Console.WriteLine("Deleting existing database...");
+            await appwriteService.DeleteDatabase(databaseId);
+        }
 
-        //         databaseResponse = await appwriteService.GetDatabase(databaseId);
-        //         databaseExists = databaseResponse.Result is not null;
-        //     }
-        //     catch (FileNotFoundException)
-        //     {
-        //         Console.WriteLine("The housing search JSON file is not valid or present within the 'DDL' directory.");
-        //         Environment.Exit(1);
-        //     }
-        //     catch (JsonException)
-        //     {
-        //         Console.WriteLine("The housing search JSON file is not valid or present within the 'DDL' directory.");
-        //         Environment.Exit(1);
-        //     }
-        // }
+        try
+        {
+            string json = File.ReadAllText(Path.Combine("DDL", "housingsearch.json"));
+            SeedDatabaseDTO? seedDatabaseDTO = JsonSerializer.Deserialize<SeedDatabaseDTO>(json);
 
-        // if (databaseExists)
-        // {
-        //     var collectionListResponse = await appwriteService.GetCollections(databaseResponse.Result);
-        //     var collectionListCount = collectionListResponse.Result.Total;
+            if (seedDatabaseDTO is null)
+            {
+                throw new JsonException();
+            }
 
-        //     Console.WriteLine($"Database '{databaseId}' has {collectionListCount} collection(s).");
+            Console.WriteLine("Creating database...");
+            await appwriteService.CreateDatabase(new DatabaseDTO { DatabaseId = databaseId, Name = seedDatabaseDTO.DatabaseName });
 
-        //     var shouldOperateOnCollection = UserSelection.GetBooleanAnswer("Would you like to operate on a collection?");
+            Console.WriteLine("Creating collections...");
+            await appwriteService.CreateCollections(databaseId, seedDatabaseDTO.Collections);
 
-        //     if (!shouldOperateOnCollection) Environment.Exit(0);
+            databaseResponse = await appwriteService.GetDatabase(databaseId);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("The housing search JSON file is not valid or present within the 'DDL' directory.");
+            Environment.Exit(1);
+        }
+        catch (JsonException)
+        {
+            Console.WriteLine("The housing search JSON file is not valid or present within the 'DDL' directory.");
+            Environment.Exit(1);
+        }
+    }
 
-        //     var collectionNames = collectionListResponse.Result.Collections.Select(c => c.Name).ToList();
+    private static async Task PromptForCollectionOperations(AppwriteService appwriteService, string databaseId, DatabaseResponse<Appwrite.Models.Database> databaseResponse)
+    {
+        var collectionListResponse = await appwriteService.GetCollections(databaseResponse.Result);
+        var collectionListCount = collectionListResponse.Result.Total;
 
-        //     var selectedCollection = UserSelection.GetSelection(collectionNames, "Select a collection:");
+        Console.WriteLine($"Database '{databaseId}' has {collectionListCount} collection(s).");
 
-        //     Console.WriteLine($"You selected: {selectedCollection}");
-        // }
+        var shouldOperateOnCollection = UserSelection.GetBooleanAnswer("Would you like to operate on a collection?");
 
-        // 1 - Generate a Listing
-        Listing listing = FakerFactory.GenerateListing();
-        var stringListing = JsonSerializer.Serialize(listing);
-        // 2 - Create a new document in the collection
-        await appwriteService.CreateDocument(databaseId, "listings", stringListing);
+        if (!shouldOperateOnCollection) Environment.Exit(0);
+
+        var collectionNames = collectionListResponse.Result.Collections.Select(c => c.Name).ToList();
+
+        var selectedCollection = UserSelection.GetSelection(collectionNames, "Select a collection:");
+
+        Console.WriteLine($"You selected: {selectedCollection}");
     }
 }
