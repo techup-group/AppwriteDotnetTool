26f21d0c50b9f8aac9900b7aad2acbe6628f39f1
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-07-31 16:12:12 +0000

    convert User json to model instance

--- AppwriteConsole.App/models/PersonModel.cs
+++ AppwriteConsole.App/models/PersonModel.cs
@@ -16,18 +16,6 @@ public class Person
   {
     string json = JsonConvert.SerializeObject(data);
     Person person = JsonConvert.DeserializeObject<Person>(json);
-
-    // Address address = (data["AddressId"] as JObject).ToObject<Address>();
-    // var person = new Person
-    // {
-    //   FirstName = data["FirstName"].ToString(),
-    //   LastName = data["LastName"].ToString(),
-    //   Email = data["Email"].ToString(),
-    //   Phone = data["Phone"].ToString(),
-    //   AccountId = data["AccountId"].ToString(),
-    //   AddressId = address
-    // };
-
     return person;
   }
 }
--- AppwriteConsole.App/models/UserModel.cs
+++ AppwriteConsole.App/models/UserModel.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace Models;
 
 public class User
@@ -6,4 +8,11 @@ public class User
   public string LastName { get; set; }
   public string Email { get; set; }
   public List<Listing> Listings { get; set; }
+
+  public static User FromJson(Dictionary<string, object> data)
+  {
+    string json = JsonConvert.SerializeObject(data);
+    User user = JsonConvert.DeserializeObject<User>(json);
+    return user;
+  }
 }
