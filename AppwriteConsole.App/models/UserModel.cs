387c6402b532b607137f222715728dd1566ad700
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-07-31 15:01:56 +0000

    define user model

---
+++ AppwriteConsole.App/models/UserModel.cs
@@ -0,0 +1,9 @@
+namespace Models;
+
+public class User
+{
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string Email { get; set; }
+  public List<Listing> Listings { get; set; }
+}
