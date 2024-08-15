133b38a55712859c31ba13910e3a7b4066c9d973
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-08-15 15:02:50 +0000

    test: faker factory model instantiation

--- AppwriteConsole.App/models/AddressModel.cs
+++ AppwriteConsole.App/models/AddressModel.cs
@@ -6,7 +6,7 @@ public class Address
 {
   public string Id { get; set; } = string.Empty;
   public string StreetAddress1 { get; set; } = string.Empty;
-  public string? StreetAddress2 { get; set; } = string.Empty;
+  public string? StreetAddress2 { get; set; }
   public string City { get; set; } = string.Empty;
   public string State { get; set; } = string.Empty;
   public string Zip { get; set; } = string.Empty;
